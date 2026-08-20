141311aa6d99c034bdeace94553ef6c383976983
bot-boi5555 <staropto1@gmail.com>
2026-08-20 15:21:39 +0300

    Added a camera

--- src/gameLayer/gameMain.cpp
+++ src/gameLayer/gameMain.cpp
@@ -1,5 +1,6 @@
 #include "gameMain.h"
 #include "assetManager.h"
+#include "gameMap.h"
 
 #include <iostream>
 #include <fstream>
@@ -8,14 +9,31 @@
 #define DELTA_TIME_TOLERANCE 		1.f/5		// max posible delta time
 
 struct GameData {
-	
+	GameMap map;
+	Camera2D camera;
+
 } gameData;
 
 AssetManager assetManager;
 
+
+
 bool initGame() {
 
+	gameData.map.create(30, 20);
+
+	gameData.map.getBlockUnsafe(0, 0).type 	= Block::dirt;
+	gameData.map.getBlockUnsafe(1, 1).type 	= Block::dirt;
+	gameData.map.getBlockUnsafe(2, 2).type 	= Block::dirt;
+	gameData.map.getBlockUnsafe(3, 3).type 	= Block::dirt;
+	gameData.map.getBlockUnsafe(4, 4).type 	= Block::dirt;
+
+	gameData.camera.target 					= {0, 0};			// puts the map (0, 0) in the center of the screen
+	gameData.camera.zoom 					= 100;
+	gameData.camera.rotation				= 0;
+
 	assetManager.loadAll();
+
 
 	return true;
 }
@@ -30,16 +48,63 @@ bool updateGame() {
 		deltaTime = DELTA_TIME_TOLERANCE;
 	}
 
+	gameData.camera.offset = {GetScreenWidth() / 2.0f, GetScreenHeight() / 2.0f}; 
 
+
+	ClearBackground({75, 75, 150, 255});
+
+	#pragma region camera movement
+
+	if (IsKeyDown(KEY_LEFT)) {
+		gameData.camera.target.x -= 7 * deltaTime;
+	}
+	if (IsKeyDown(KEY_RIGHT)) {
+		gameData.camera.target.x += 7 * deltaTime;
+	}
+	if (IsKeyDown(KEY_UP)) {
+		gameData.camera.target.y -= 7 * deltaTime;
+	}
+	if (IsKeyDown(KEY_DOWN)) {
+		gameData.camera.target.y += 7 * deltaTime;
+	}
+
+	#pragma endregion
+
+	BeginMode2D(gameData.camera);
+
+
+	for (int y = 0; y < gameData.map.height; ++y) {
+		
+		for (int x = 0; x < gameData.map.width; ++x) {
+
+			auto& block = gameData.map.getBlockUnsafe(x, y);
+
+			if (block.type == Block::air) {
+				continue;
+			}
+
+			float size = 1;
+			float posX = x * size;
+			float posY = y * size;
+			
+
+			DrawTexturePro(
+				assetManager.dirt, 
+				{0, 0, (float) assetManager.dirt.width, (float) assetManager.dirt.height}, 	// {posX, posY, sizeX, sizeY} (what part of the texture to use from the original png)
+				{posX, posY, size, size},													// {posX, posY, sizeX, sizeY} (position and size of the texture to be drawn on the window)
+				{0, 0},																		// origin (top left corner)
+				0,																			// rotation in degrees
+				WHITE																		// tint
+			);
+
+
+		}
 	
-	DrawTexturePro(
-		assetManager.dirt, 
-		{0, 0, (float) assetManager.dirt.width, (float) assetManager.dirt.height}, 	// {posX, posY, sizeX, sizeY} (what part of the texture to use from the original png)
-		{50, 50, 100, 100},																	// {posX, posY, sizeX, sizeY} (position and size of the texture to be drawn on the window)
-		{},
-		0,
-		WHITE
-	);
+	}
+
+
+	EndMode2D();	
+	
 
 	return true;
 }
--- src/gameLayer/gameMap.cpp
+++ src/gameLayer/gameMap.cpp
@@ -4,5 +4,39 @@
 
 
 void GameMap::create(int width, int height) {
+
+	*this = {}; // clear all data
+
+	mapData.resize(width * height);
+
+	this->width = width;
+	this->height = height;
+
+	// auto will automatically detect the type (Texture)
+	for (auto& e : mapData) {
+		e = {};					// clear block data
+	}
+
+}
+
+
+Block& GameMap::getBlockUnsafe(int x, int y) {
+
+	permaAssertCommentDevelopement(mapData.size() == width * height, "Map not initialized.");
+
+	permaAssertCommentDevelopement(x >= 0 && y >= 0 && x < width && y < height, "Out of bounds error.");
+
+	return mapData[y * width + x];
+}
+
+
+Block* GameMap::getBlockSafe(int x, int y) {
 	
+	permaAssertCommentDevelopement(mapData.size() == width * height, "Map not initialized.");
+
+	if (x < 0 || y < 0 || x >= width || y >= height) {
+		return nullptr;
+	}
+
+	return &mapData[y * width + x];
 }
